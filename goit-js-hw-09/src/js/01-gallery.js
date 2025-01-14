69f1acc12783d47a9a474bfca38cec1d1fcb29fd
Osman <157528072+osmnr@users.noreply.github.com>
2025-01-14 12:26:46 +0300

    updated
    updated

--- goit-js-hw-09/src/js/01-gallery.js
+++ goit-js-hw-09/src/js/01-gallery.js
@@ -1,18 +1,57 @@
 import { images } from '../img/images';
 
-const galleryContainer = document.querySelector(".gallery");
+const gallery = document.querySelector(".gallery");
 
 const galleryMarkup = images.map(
-    ({preview, original, description}) => `
-    <li class="gallery-item">
-        <a class="gallery-link" href="${original.substring(1, original.length-1)}">
-            <img class="gallery-image" 
-            src="${preview.substring(1, preview.length-1)}"
-            alt="${description}"
+  ({ preview, original, description }) =>
+      `<li class="gallery-item">
+          <a class="gallery-link" href="${original.substring(
+            1,
+            original.length - 1
+          )}">
+            <img
+              class="gallery-image"
+              src="${preview.substring(1, preview.length - 1)}"
+              data-source="${original.substring(1, original.length - 1)}"
+              alt="${description}"
             />
-        </a>
-    </li>
-`    
-).join("");
+          </a>
+        </li>`
+  ).join("");
 
-galleryContainer.innerHTML = galleryMarkup;
+gallery.innerHTML = galleryMarkup;
+
+const img = document.getElementsByTagName("IMG");
+for (var i = 0; i < img.length; i++) {
+  // adding event listener to the 'img' to make the size bigger when hovered
+  img[i].addEventListener("mouseenter", (event) => {
+    event.target.style.transform = "scale(1.04)";
+  });
+  //  adding event listener to the 'img' to remove the changes of the size
+  img[i].addEventListener("mouseleave", (event) => {
+    event.target.style.transform = "scale(1)"; // Reset scale
+  });
+}
+
+
+gallery.addEventListener("click", imageClick);
+
+function imageClick(event) {
+  event.preventDefault();
+  const imageData = event.target;
+  if (imageData.tagName !== "IMG") return;
+
+  const instance = basicLightbox.create(
+    `<img width="1400" height="900" src="${imageData.dataset.source}">`
+  );
+
+  instance.show();
+  
+  function handleKeydown(event) {
+    if (event.key === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", handleKeydown);
+    }
+  }
+  document.addEventListener("keydown", handleKeydown);
+}
